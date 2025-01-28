6178d500d2ae95f086ca8141234163ec133760f0
Sreenidhi <125287004+Nidhi123456N@users.noreply.github.com>
2025-01-28 20:34:56 +0530

    Add files via upload

---
+++ Rhinoplasty.ai/constants.py
@@ -0,0 +1,5 @@
+PROMPTS = [
+    "A photorealistic side profile of a person with a narrower and straighter nose",
+    "A front portrait of a person with a small, sharp nasal tip",
+    "A side profile of a person with a well-defined nasal bridge"
+]
