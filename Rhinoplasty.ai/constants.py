aa003cf74f346aafa7e4f15d6c0e09ae47a216c6
Sreenidhi <125287004+Nidhi123456N@users.noreply.github.com>
2025-01-28 21:08:06 +0530

    Delete Rhinoplasty.ai directory

--- Rhinoplasty.ai/constants.py
+++
@@ -1,5 +0,0 @@
-PROMPTS = [
-    "A photorealistic side profile of a person with a narrower and straighter nose",
-    "A front portrait of a person with a small, sharp nasal tip",
-    "A side profile of a person with a well-defined nasal bridge"
-]
